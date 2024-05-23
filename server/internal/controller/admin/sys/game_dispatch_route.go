36cedb98ff997e2e3ab1288561aaa5b0dd7ed1cb
胡文俊(Hu WenJun/星云) <hu.wenjun1@olaola.chat>
2024-05-23 16:40:39 +0800

    游戏路由改为采用nginx实现

--- server/internal/controller/admin/sys/game_dispatch_route.go
+++ server/internal/controller/admin/sys/game_dispatch_route.go
@@ -6,153 +6,139 @@
 // @AutoGenerate Version 2.9.8
 package sys
 
-import (
-	"context"
-	"errors"
-	"fmt"
-	"log"
-	"net/http"
-	"net/http/httputil"
-	"net/url"
-	"strings"
-
-	"github.com/gogf/gf/v2/container/gmap"
-	"github.com/gogf/gf/v2/frame/g"
-	"hotgo/internal/library/location"
-	"hotgo/internal/service"
-)
-
-var proxyPool = gmap.NewStrAnyMap(true)
-
-type GameReverseProxy struct {
-	*httputil.ReverseProxy
-	isDirect bool
-	url      *url.URL
-}
-
-// NewProxy takes target host and creates a reverse proxy
-func NewProxy(factoryType string, targetHost string, isDirect bool) (*GameReverseProxy, error) {
-	url, err := url.Parse(targetHost)
-	if err != nil {
-		return nil, err
-	}
-	p := &GameReverseProxy{isDirect: isDirect, url: url}
-	proxy := httputil.NewSingleHostReverseProxy(url)
-	p.ReverseProxy = proxy
-	originalDirector := proxy.Director
-	proxy.Director = func(req *http.Request) {
-		p.modifyRequest(factoryType, req)
-		originalDirector(req)
-		if p.isDirect {
-			req.URL = p.url
-		}
-	}
-
-	proxy.ModifyResponse = p.modifyResponse()
-	proxy.ErrorHandler = p.errorHandler()
-	return p, nil
-}
-
-func (p *GameReverseProxy) modifyRequest(factoryType string, req *http.Request) {
-	index := strings.Index(req.URL.Path, factoryType)
-	if index >= 0 {
-		req.URL.Path = req.URL.Path[index+len(factoryType):]
-		req.Header.Set("X-Proxy", "Simple-Reverse-Proxy")
-	}
-}
-
-func (p *GameReverseProxy) errorHandler() func(http.ResponseWriter, *http.Request, error) {
-	return func(w http.ResponseWriter, req *http.Request, err error) {
-		fmt.Printf("Got error while modifying response: %v \n", err)
-		return
-	}
-}
-
-func (p *GameReverseProxy) modifyResponse() func(*http.Response) error {
-	return func(resp *http.Response) error {
-		return nil
-		return errors.New("response body is invalid")
-	}
-}
-
-// ProxyRequestHandler handles the http request using proxy
-func ProxyRequestHandler(proxy *httputil.ReverseProxy) func(http.ResponseWriter, *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		proxy.ServeHTTP(w, r)
-	}
-}
-func route(w http.ResponseWriter, req *http.Request) {
-	path := req.URL.Path
-	path = path[1:]
-	if len(path) == 0 {
-		ctx := context.TODO()
-		ip, err := location.GetLocalIP()
-		if err != nil {
-			g.Log().Error(ctx, err)
-			return
-		}
-		http.Redirect(w, req, fmt.Sprintf("http://%s%s/web", ip, g.Cfg().MustGet(ctx, "server.address").String()), http.StatusFound)
-		return
-	}
-	index := strings.Index(path, "/")
-	var factoryType string
-	if index > 0 {
-		factoryType = path[:index]
-	} else {
-		factoryType = path
-	}
-	isRawDispatch := false
-	p := proxyPool.GetOrSetFunc(factoryType, func() interface{} {
-		data, err := service.SysGameRoute().GetGameRouteByFactory(context.TODO(), factoryType)
-		if err != nil || data == nil {
-			return nil
-		}
-		if data.Url == "raw" {
-			factoryType = req.URL.Path
-			isRawDispatch = true
-			return nil
-		}
-		proxy, err := NewProxy(factoryType, data.Url, false)
-		if err != nil {
-			panic(err)
-		}
-		return proxy
-	})
-	if isRawDispatch {
-		p = proxyPool.GetOrSetFunc(factoryType, func() interface{} {
-			data, err := service.SysGameRoute().GetGameRouteByFactory(context.TODO(), factoryType)
-			if err != nil || data == nil {
-				return nil
-			}
-			if data != nil {
-				proxy, err := NewProxy(factoryType, data.Url, true)
-				if err != nil {
-					panic(err)
-				}
-				return proxy
-			}
-			return nil
-		})
-	}
-
-	if p != nil {
-		proxy := p.(*GameReverseProxy)
-		proxy.ServeHTTP(w, req)
-
-	} else {
-		_, err := w.Write([]byte("请检查房型是否正确或存在"))
-		if err != nil {
-			return
-		}
-	}
-
-}
+//
+//var proxyPool = gmap.NewStrAnyMap(true)
+//
+//type GameReverseProxy struct {
+//	*httputil.ReverseProxy
+//	isDirect bool
+//	url      *url.URL
+//}
+//
+//// NewProxy takes target host and creates a reverse proxy
+//func NewProxy(factoryType string, targetHost string, isDirect bool) (*GameReverseProxy, error) {
+//	url, err := url.Parse(targetHost)
+//	if err != nil {
+//		return nil, err
+//	}
+//	p := &GameReverseProxy{isDirect: isDirect, url: url}
+//	proxy := httputil.NewSingleHostReverseProxy(url)
+//	p.ReverseProxy = proxy
+//	originalDirector := proxy.Director
+//	proxy.Director = func(req *http.Request) {
+//		p.modifyRequest(factoryType, req)
+//		originalDirector(req)
+//		if p.isDirect {
+//			req.URL = p.url
+//		}
+//	}
+//
+//	proxy.ModifyResponse = p.modifyResponse()
+//	proxy.ErrorHandler = p.errorHandler()
+//	return p, nil
+//}
+//
+//func (p *GameReverseProxy) modifyRequest(factoryType string, req *http.Request) {
+//	index := strings.Index(req.URL.Path, factoryType)
+//	if index >= 0 {
+//		req.URL.Path = req.URL.Path[index+len(factoryType):]
+//		req.Header.Set("X-Proxy", "Simple-Reverse-Proxy")
+//	}
+//}
+//
+//func (p *GameReverseProxy) errorHandler() func(http.ResponseWriter, *http.Request, error) {
+//	return func(w http.ResponseWriter, req *http.Request, err error) {
+//		fmt.Printf("Got error while modifying response: %v \n", err)
+//		return
+//	}
+//}
+//
+//func (p *GameReverseProxy) modifyResponse() func(*http.Response) error {
+//	return func(resp *http.Response) error {
+//		return nil
+//		return errors.New("response body is invalid")
+//	}
+//}
+//
+//// ProxyRequestHandler handles the http request using proxy
+//func ProxyRequestHandler(proxy *httputil.ReverseProxy) func(http.ResponseWriter, *http.Request) {
+//	return func(w http.ResponseWriter, r *http.Request) {
+//		proxy.ServeHTTP(w, r)
+//	}
+//}
+//func route(w http.ResponseWriter, req *http.Request) {
+//	path := req.URL.Path
+//	path = path[1:]
+//	if len(path) == 0 {
+//		ctx := context.TODO()
+//		ip, err := location.GetLocalIP()
+//		if err != nil {
+//			g.Log().Error(ctx, err)
+//			return
+//		}
+//		http.Redirect(w, req, fmt.Sprintf("http://%s%s/web", ip, g.Cfg().MustGet(ctx, "server.address").String()), http.StatusFound)
+//		return
+//	}
+//	index := strings.Index(path, "/")
+//	var factoryType string
+//	if index > 0 {
+//		factoryType = path[:index]
+//	} else {
+//		factoryType = path
+//	}
+//	isRawDispatch := false
+//	p := proxyPool.GetOrSetFunc(factoryType, func() interface{} {
+//		data, err := service.SysGameRoute().GetGameRouteByFactory(context.TODO(), factoryType)
+//		if err != nil || data == nil {
+//			return nil
+//		}
+//		if data.Url == "raw" {
+//			factoryType = req.URL.Path
+//			isRawDispatch = true
+//			return nil
+//		}
+//		proxy, err := NewProxy(factoryType, data.Url, false)
+//		if err != nil {
+//			panic(err)
+//		}
+//		return proxy
+//	})
+//	if isRawDispatch {
+//		p = proxyPool.GetOrSetFunc(factoryType, func() interface{} {
+//			data, err := service.SysGameRoute().GetGameRouteByFactory(context.TODO(), factoryType)
+//			if err != nil || data == nil {
+//				return nil
+//			}
+//			if data != nil {
+//				proxy, err := NewProxy(factoryType, data.Url, true)
+//				if err != nil {
+//					panic(err)
+//				}
+//				return proxy
+//			}
+//			return nil
+//		})
+//	}
+//
+//	if p != nil {
+//		proxy := p.(*GameReverseProxy)
+//		proxy.ServeHTTP(w, req)
+//
+//	} else {
+//		_, err := w.Write([]byte("请检查房型是否正确或存在"))
+//		if err != nil {
+//			return
+//		}
+//	}
+//
+//}
 
 func LanuchGameRouteServer() {
 	// initialize a reverse proxy and pass the actual backend server url here
 
 	// handle all requests to your server using the proxy
-	http.HandleFunc("/", route)
-	address := g.Cfg().MustGet(context.TODO(), "server.RouteMapAddress").String()
-	log.Fatal(http.ListenAndServe(address, nil))
+	//http.HandleFunc("/", route)
+	//address := g.Cfg().MustGet(context.TODO(), "server.RouteMapAddress").String()
+	//log.Fatal(http.ListenAndServe(address, nil))
+	ReloadRoute()
 }
--- server/internal/controller/admin/sys/game_route.go
+++ server/internal/controller/admin/sys/game_route.go
@@ -7,9 +7,18 @@
 package sys
 
 import (
+	"bytes"
 	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"runtime"
+	"strings"
+	"text/template"
 
 	"hotgo/api/admin/gameroute"
+	"hotgo/internal/model/input/sysin"
 	"hotgo/internal/service"
 )
 
@@ -41,7 +50,9 @@ func (c *cGameRoute) Export(ctx context.Context, req *gameroute.ExportReq) (res 
 // Edit 更新游戏路由
 func (c *cGameRoute) Edit(ctx context.Context, req *gameroute.EditReq) (res *gameroute.EditRes, err error) {
 	err = service.SysGameRoute().Edit(ctx, &req.GameRouteEditInp)
-	proxyPool.Remove(req.GameRouteEditInp.FactoryType)
+	//proxyPool.Remove(req.GameRouteEditInp.FactoryType)
+	ReloadRoute()
+
 	return
 }
 
@@ -60,7 +71,105 @@ func (c *cGameRoute) View(ctx context.Context, req *gameroute.ViewReq) (res *gam
 // Delete 删除游戏路由
 func (c *cGameRoute) Delete(ctx context.Context, req *gameroute.DeleteReq) (res *gameroute.DeleteRes, err error) {
 	err = service.SysGameRoute().Delete(ctx, &req.GameRouteDeleteInp)
-	proxyPool.Remove(req.FactoryType)
-
+	//proxyPool.Remove(req.FactoryType)
+	ReloadRoute()
 	return
+}
+
+type Data struct {
+	Data    []Item
+	ZipData []Item
+	Port    int
+}
+
+type Item struct {
+	Name  string
+	Addrs string
+}
+
+var tmpl *template.Template
+
+func init() {
+	var err error
+	bs, err := os.ReadFile("./manifest/config/nginx.conf")
+	if err != nil {
+		panic(err)
+	}
+	tmpl, err = template.New("test").Parse(string(bs))
+	if err != nil {
+		panic(err)
+	}
+
+}
+func ReloadRoute() {
+	all, err := service.SysGameRoute().Model(context.TODO()).All()
+	if err != nil {
+		panic(err)
+	}
+	var list []*sysin.GameRouteListModel
+	err = all.Structs(&list)
+	if err != nil {
+		panic(err)
+	}
+	err = makeConf(list)
+	if err != nil {
+		panic(err)
+
+	}
+
+}
+func makeConf(conf []*sysin.GameRouteListModel) error {
+	if len(conf) == 0 {
+		return nil
+	}
+	//生成nginx配置文件
+	data := &Data{
+		Port:    4399,
+		Data:    []Item{},
+		ZipData: []Item{},
+	}
+	for _, route := range conf {
+		if route.Url != "raw" {
+			if strings.HasSuffix(route.FactoryType, ".zip") {
+				data.ZipData = append(data.ZipData, Item{
+					Name:  route.FactoryType,
+					Addrs: route.Url,
+				})
+			} else {
+
+				data.Data = append(data.Data, Item{
+					Name:  route.FactoryType,
+					Addrs: route.Url,
+				})
+			}
+
+		}
+	}
+
+	var buf bytes.Buffer
+	err := tmpl.Execute(&buf, data)
+	if err != nil {
+		return err
+	}
+	err = os.WriteFile("/home/ecs-user/.local/etc/nginx/sites-enabled/nginx.game.route.proxy.conf", buf.Bytes(), 0666)
+	//err = os.WriteFile("/opt/homebrew/etc/nginx/conf.d/nginx.game.route.proxy.conf", buf.Bytes(), 0666)
+	if err != nil {
+		return err
+	}
+	//reload nginx
+	//ubuntu 默认安装位置
+	var cmd *exec.Cmd
+	if runtime.GOOS == "darwin" {
+		cmd = exec.Command("brew", "services", "reload", "nginx")
+	} else if runtime.GOOS == "linux" {
+		cmd = exec.Command("sudo", "/usr/sbin/nginx", "-s", "reload")
+	} else {
+		return fmt.Errorf("unknow os type")
+	}
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		return err
+	}
+	log.Println("nginx output\n", string(out))
+	return nil
 }
